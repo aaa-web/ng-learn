869c1c43e1a2d7fd490f30d60ac82ea5c300f491
andres.castro <andres.castro@pragma.com.co>
2019-09-17 19:24:50 -0500

    moving routing to rught module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ConversationComponent } from './conversation/conversation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'conversation', component: ConversationComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
