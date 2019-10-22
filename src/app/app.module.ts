02f5bba822f0c52a99179f01e3b0c5b4a8758500
andres.castro <andres.castro@pragma.com.co>
2019-10-22 16:31:06 -0500

    adding a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { HomeComponent } from './home/home.component';
 import { ConversationComponent } from './conversation/conversation.component';
 import { ProfileComponent } from './profile/profile.component';
 import { MenuComponent } from './menu/menu.component';
+import { SearchPipe } from './pipes/search';
+
 
 @NgModule({
   declarations: [
@@ -17,12 +20,14 @@ import { MenuComponent } from './menu/menu.component';
     HomeComponent,
     ConversationComponent,
     ProfileComponent,
-    MenuComponent
+    MenuComponent, 
+    SearchPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { UserService } from '../services/user/user.service';
 export class HomeComponent implements OnInit {
 
   friends: User[];
+  query: string = '';
 
   constructor( private userService: UserService ) {
     
