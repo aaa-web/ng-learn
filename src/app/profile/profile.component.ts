6400f24f2c8a1d90c2c90f15f25668a146062d79
andres.castro <andres.castro@pragma.com.co>
2019-11-06 14:16:03 -0500

    profile dynamic data

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user/user.service';
+import { User } from '../interfaces/user';
+import { AuthenticationService } from '../services/auth/authentication.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+    ) { 
+      this.getSesionUser();
+    }
 
   ngOnInit() {
+  }
+  
+  getSesionUser() {
+    this.authenticationService.getSesionStatus().subscribe(
+      (status) => {
+        this.userService.getUserById(status.uid).valueChanges().subscribe(
+          (data: User) => {
+            this.user = data;
+            console.log(data);            
+          }, 
+          (error) => {
+            console.log(error);    
+          }
+        );
+      },
+      (error) => {
+        console.log(error);        
+      }
+    );
+  }
+
+  saveSettings() {
+    this.userService.editUser(this.user).then(
+      () => {
+        console.log('Cambios guardados con éxito');        
+      }
+    ).catch(
+      (error) => {
+        console.log(error);        
+      }
+    );
   }
 
 }
