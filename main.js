b1a17c411187407537795710030bc36792f71320
unknown <andrec230@gmail.com>
2019-11-10 21:32:23 -0500

    packagging app

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let win;
 function createWindow() {
     win = new BrowserWindow({width: 800, height: 600});
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'dist/retro-chat/index.html'),
+        pathname: path.join(__dirname, 'dist/platzinger/index.html'),
         protocol: 'file:',
         slashes: true
     }));
@@ -21,5 +21,11 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     if(process.platform !== 'darwin') {
         app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if(win === null) {
+        createWindow();
     }
 });
